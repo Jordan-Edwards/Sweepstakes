ae0e5e9bd6f412719a416200cfea1a620dd76683
Jordan-Edwards <edwardsjordan441@gmail.com>
2021-06-23 13:59:43 -0500

    Modified the Contestant Class parameters to work in tandom with the previously commited @statmethods in 'user_interface.py'

--- contestant.py
+++ contestant.py
@@ -1,10 +1,18 @@
+from user_interface import UserInterface
+from sweepstakes import Sweepstakes
+
+
 class Contestant:
 
-    def __init__(self, first_name, last_name, email, address, registration_number):
-        self.first_name = first_name
-        self.last_name = last_name
-        self.email = email
-        self.registration_number = registration_number
+    def __init__(self):
+        self.first_name = UserInterface.get_user_string("Enter your Legal First Name")
+        self.last_name = UserInterface.get_user_string("Enter your Legal Last Name")
+        self.email = UserInterface.get_user_string("Enter the Email Address You Would like us to use to Contact you")
+        self.registration_number = UserInterface.get_user_int(int)
 
     def notification(self, is_winner):
-        pass
+        is_winner = False
+        if is_winner:
+            print("You are Not Today's Luck Winner. Maybe Next Time!")
+        else:
+            print("Congratulations! You are Today's Lucky Winner!")
